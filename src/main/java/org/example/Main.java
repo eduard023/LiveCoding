475fa3b10f003706f6a19df8f230d37d9ead2e77
eduard023 <edo.avdzh@yandex.ru>
2023-04-05 22:40:37 +0300

    Готова

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Array array = new Array();
-        int[] number = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        System.out.println(array.modifyArray(number));
+        /* Необходимо реализовать следующий метод:
+        1. Получаем на входе массив чисел
+        2. Все четные числа увеличиваем на единицу
+        3. Возвращаем кусок списка с 3-го по 7-й элемент*/
+
+        int[] number = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        System.out.println(Arrays.toString(modifyArray(number)));
 
 
     }
-}
+
+    static int[] modifyArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0){
+                array[i]++;
+            }
+
+        }
+        int[] modifyNumber = new int[5];
+        System.arraycopy(array, 2, modifyNumber, 0, 5);
+        return modifyNumber;
+    }
+ }
